90ddd0f9fcc338074d62b08d764ceecc30bc97ea
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-04-28 19:46:53 +0530

    JS D8

---
+++ 25_Console_Objects.js
@@ -0,0 +1,11 @@
+console.log(console);
+console.clear();
+console.log("Harshal");
+console.error("This is an error");  console.assert(5>3); 
+
+obj = {a: 1,b: 2,c: 3}
+console.table(obj);
+console.warn("Don't eat fast food.");
+console.info("The warning above is very important info.");
+console.time("a");
+console.timeEnd("a");
