b2450f3b96cf63738209c7a7f893de9aafcb06ef
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-04-30 09:06:12 +0530

    JS D10

---
+++ 27_DOM,BOM&Windows_Object.js
@@ -0,0 +1,12 @@
+// Window Object : It represent browser window and provides method to control it.It is a Global Object
+console.log(window);
+
+// DOM : Document Object Model.
+         // It represents the page content as HTML.
+console.log(document.body);
+document.body.style.background = "Cyan"
+
+// BOM : Browser Object Model
+         // The Browser Object Model represent additional object provided by the browser (host environment) for working with everything except the document.
+         // The function alert/Confirm/prompt are also a part of the BOM.
+location.href = "https://www.youtube.com/"
